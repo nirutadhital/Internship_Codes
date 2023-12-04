d4896a98ff1b7509edbee26d0b6a1019376f18ee
niruta dhital <dhitalniruta99@gmail.com>
2023-12-04 22:41:17 +0545

    final commit

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -103,5 +103,19 @@ class Coke : ColdDrink
 }
 class Meal
 {
+    private List<Item> items = new ArrayList<Item>();	
+
+   public void addItem(Item item){
+      items.add(item);
+   }
+
+   public float getCost(){
+      float cost = 0.0f;
+      
+      for (Item item : items) {
+         cost += item.price();
+      }		
+      return cost;
+   }
     
 }
